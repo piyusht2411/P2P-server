05164e12117db05bfdecdfe4c8003b00907e9cef
Piyush Thakur <90854455+piyusht2411@users.noreply.github.com>
2024-03-13 23:31:34 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,15 +17,6 @@ declare global {
 const app: Application = express();
 const PORT = process.env.PORT || 8080;
 //middleware
- res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
 var corsOptions = {
   origin: "https://p2p-client-blue.vercel.app",
   credentials: true
