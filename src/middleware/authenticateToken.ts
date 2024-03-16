49d5e6231793f28ec986c8da7a5bfe96334696c3
Piyush Thakur <90854455+piyusht2411@users.noreply.github.com>
2024-03-16 11:39:04 +0530

    Update authenticateToken.ts

--- src/middleware/authenticateToken.ts
+++ src/middleware/authenticateToken.ts
@@ -11,6 +11,9 @@ export const authenticateToken = async(req: Request, res: Response, next: NextFu
     // const mmy = req.params.id; => may check this this may be correct
 
     const authToken =  req.header('authorization');
+        if(!authToken){
+        return res.status(405).json({message : "hey "})
+    }
     // if (!authToken) {
     //     return res.status(401).send('Access token not found');
     // }
